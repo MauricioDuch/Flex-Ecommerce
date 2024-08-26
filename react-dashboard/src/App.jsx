ce1d96d5b8da985829e86ec0bc9d45b35ffb94ab
MauricioDuch <mauricioduch06@gmail.com>
2024-08-26 14:16:41 -0300

    subida del proyecto

--- react-dashboard/src/App.jsx
+++ react-dashboard/src/App.jsx
@@ -1,35 +1,33 @@
 import React, { Component } from "react";
-import UserList from './components/UserList.jsx';
-import ProductList from "./components/TotalProducts.jsx";
+import UserList from "./components/userList.jsx";
+import ProductList from "./components/totalProducts.jsx";
 import Home from "./components/Home.jsx";
 import SingleProduct from "./components/SingleProduct.jsx";
 import SingleUser from "./components/SingleUser.jsx";
-import LastProduct from "./components/LastProduct.jsx";
-import LastUser from "./components/LastUser.jsx";
+import LastProduct from "./components/lastProduct.jsx";
+import LastUser from "./components/lastUser.jsx";
 import EachCategory from "./components/EachCategory.jsx";
-import {Route, Switch} from "react-router-dom";
-import styles from "./app.module.css"
+import { Route, Switch } from "react-router-dom";
+import styles from "./app.module.css";
 import Header from "./components/header.jsx";
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className={styles.App}>
-
-       <Header/>     
-       <Switch >
-        <Route path="/productList" exact component={ProductList}/>
-        <Route path="/products/:id" exact component={SingleProduct}/>
-        <Route path="/userList" exact component={UserList}/>
-        <Route path="/user/:id" exact component={SingleUser}/>
-        <Route path="/lastProduct" exact component={LastProduct}/>
-        <Route path="/eachCategory" exact component={EachCategory}/>
-        <Route path="/lastuser" exact component={LastUser}/>
-        <Route path="/" exact component={Home}/> 
-      </Switch>
+        <Header />
+        <Switch>
+          <Route path="/productList" exact component={ProductList} />
+          <Route path="/products/:id" exact component={SingleProduct} />
+          <Route path="/userList" exact component={UserList} />
+          <Route path="/user/:id" exact component={SingleUser} />
+          <Route path="/lastProduct" exact component={LastProduct} />
+          <Route path="/eachCategory" exact component={EachCategory} />
+          <Route path="/lastuser" exact component={LastUser} />
+          <Route path="/" exact component={Home} />
+        </Switch>
       </div>
     );
   }
-    
 }
 
 export default App;
--- react-dashboard/src/components/header.jsx
+++ react-dashboard/src/components/header.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import styles from "./header.module.css"
+import styles from "./header.module.css";
 export default class Header extends Component {
-render(){
-    return(
-        <div>
-            
-           <header className={styles.header}>
-           <a href="http://localhost:3030"><img className={styles.img} src="/favicon.ico" alt="Logo" /></a> 
-            <h1>Bienvenidos al Dashboard de Futbol XI</h1>
-            </header>
-           
-        </div>
-        
-    )
-}
+  render() {
+    return (
+      <div>
+        <header className={styles.header}>
+          <a href="http://localhost:3030">
+            <img className={styles.img} src="/logo-futbolXI.png" alt="Logo" />
+          </a>
+          <h1>Bienvenidos al Dashboard de Futbol XI</h1>
+        </header>
+      </div>
+    );
+  }
 }
